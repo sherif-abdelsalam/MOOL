0f292dcab385707e7de62171f04e222653224528
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-22 19:45:44 +0200

    reusable widgets

---
+++ lib/widgets/reuse/custom_scaffold_header.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:mool/icons/arrow_back_icon.dart';
+
+class CustomScaffoldHeader extends StatelessWidget {
+  const CustomScaffoldHeader(
+      {super.key, required this.title, required this.bodyContent, this.trailing});
+
+  final Widget bodyContent;
+  final String title;
+  final Widget? trailing;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 0,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 48,
+                color: const Color(0xFF292D32),
+              ),
+              Positioned(
+                left: 8,
+                right: 16,
+                child: Row(
+                  children: [
+                    ArrowBackIcon(),
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.titleMedium!.fontSize,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    if(trailing != null) trailing!,
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: bodyContent,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/reuse/custom_scaffold_header_home.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomScaffoldHeaderHome extends StatefulWidget {
+  const CustomScaffoldHeaderHome({super.key, required this.bodyContent});
+
+  final Widget bodyContent;
+
+  @override
+  State<CustomScaffoldHeaderHome> createState() =>
+      _CustomScaffoldHeaderHomeState();
+}
+
+class _CustomScaffoldHeaderHomeState extends State<CustomScaffoldHeaderHome> {
+  bool isWomenSelected = true; // Default selection
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 0,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 94,
+                color: const Color(0xFF292D32),
+              ),
+              Positioned(
+                left: 16,
+                right: 16,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          "images/mool_logo.png",
+                          width: 100,
+                        ),
+                        Spacer(),
+                        SvgPicture.asset("images/search-normal.svg"),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        SvgPicture.asset("images/notification.svg"),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildTabButton(
+                          title: 'WOMEN',
+                          isSelected: isWomenSelected,
+                          onTap: () {
+                            setState(() {
+                              isWomenSelected = true;
+                            });
+                          },
+                        ),
+                        SizedBox(width: 32),
+                        _buildTabButton(
+                          title: 'MEN',
+                          isSelected: !isWomenSelected,
+                          onTap: () {
+                            setState(() {
+                              isWomenSelected = false;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(child: widget.bodyContent),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTabButton({
+    required String title,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: isSelected
+                  ? const Color.fromARGB(255, 255, 255, 255)
+                  : const Color(0xFFCCCCCC),
+              fontSize: 14,
+              // fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+          SizedBox(height: 3),
+          Container(
+            height: 3,
+            width: 40,
+            color: isSelected ? Colors.white : Colors.transparent,
+          ),
+        ],
+      ),
+    );
+  }
+}
