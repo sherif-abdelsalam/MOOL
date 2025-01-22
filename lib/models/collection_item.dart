39d2823fa7257630bf3d282e626991e4adddca2c
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-22 19:44:04 +0200

    collection model

---
+++ lib/models/collection_item.dart
@@ -0,0 +1,11 @@
+class CollectionItem {
+  final String title;
+  final String imageUrl;
+  final String buttonText;
+
+  CollectionItem({
+    required this.title,
+    required this.imageUrl,
+    required this.buttonText,
+  });
+}
