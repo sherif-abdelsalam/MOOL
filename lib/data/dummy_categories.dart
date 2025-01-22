004cb9984ecf71f58f79bd3f428ee1b986fe2b0e
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-22 19:43:23 +0200

    dummy data

---
+++ lib/data/dummy_categories.dart
@@ -0,0 +1,37 @@
+import 'package:mool/models/category.dart';
+import 'package:mool/models/collection_item.dart';
+
+final categories = [
+  Category(name: 'Tops', imageUrl: 'images/tops.png'),
+  Category(name: 'Bottoms', imageUrl: 'images/bottoms.png'),
+  Category(name: 'Dresses', imageUrl: 'images/dresses.png'),
+];
+
+final sliderCategories = [
+  Category(name: 'SALE', imageUrl: 'images/flash_sale.png'),
+  Category(name: 'View All', imageUrl: 'images/view_all.png'),
+  Category(name: 'Dresses', imageUrl: 'images/dresses.png'),
+  Category(name: 'Tops', imageUrl: 'images/tops.png'),
+  Category(name: 'Bottoms', imageUrl: 'images/bottoms.png'),
+  Category(name: 'T-Shirts', imageUrl: 'images/tshirts.png'),
+  Category(name: 'SALE', imageUrl: 'images/sale.png'),
+  Category(name: 'Nightwear', imageUrl: 'images/nightwear.png'),
+];
+
+final collectionItems = [
+  CollectionItem(
+    title: 'NEW SUMMER COLLECTION 2023',
+    imageUrl: 'images/collection_1.png',
+    buttonText: 'Shop now',
+  ),
+  CollectionItem(
+    title: 'NEW SUMMER COLLECTION 2023',
+    imageUrl: 'images/collection_2.png',
+    buttonText: 'Watch now',
+  ),
+  CollectionItem(
+    title: 'NEW SUMMER COLLECTION 2023',
+    imageUrl: 'images/collection_1.png',
+    buttonText: 'Shop now',
+  ),
+];
