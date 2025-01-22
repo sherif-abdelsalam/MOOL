97c44d8b012670e0d001ec00845c4d07023faf40
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-22 12:06:02 +0200

    text input

---
+++ lib/widgets/singing/text_input.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mool/widgets/singing/input_filed_deocration.dart';
+
+class TextInput extends StatelessWidget {
+  const TextInput(
+      {super.key, required this.labelText, required this.onSaveInput});
+  final String labelText;
+  final void Function(String input) onSaveInput;
+
+  String? _validator(String? value) {
+    if (value == null ||
+        value.isEmpty ||
+        value.trim().length <= 1 ||
+        value.trim().length > 50) {
+      return 'Must be between 1 and 50 characters.';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InputFiledDecoration(
+      isPassword: false,
+        labelText: labelText, onSaveInput: onSaveInput, validator: _validator);
+  }
+}
