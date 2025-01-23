2573ffc5c7ff638154add3d50ca7b923c502e7d3
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-23 02:17:34 +0200

    updated data

--- lib/data/dummy_categories.dart
+++ lib/data/dummy_categories.dart
@@ -1,37 +1,85 @@
+import 'package:flutter/material.dart';
 import 'package:mool/models/category.dart';
 import 'package:mool/models/collection_item.dart';
+import 'package:mool/models/product.dart';
 
 final categories = [
-  Category(name: 'Tops', imageUrl: 'images/tops.png'),
-  Category(name: 'Bottoms', imageUrl: 'images/bottoms.png'),
-  Category(name: 'Dresses', imageUrl: 'images/dresses.png'),
-];
-
-final sliderCategories = [
-  Category(name: 'SALE', imageUrl: 'images/flash_sale.png'),
-  Category(name: 'View All', imageUrl: 'images/view_all.png'),
-  Category(name: 'Dresses', imageUrl: 'images/dresses.png'),
-  Category(name: 'Tops', imageUrl: 'images/tops.png'),
-  Category(name: 'Bottoms', imageUrl: 'images/bottoms.png'),
-  Category(name: 'T-Shirts', imageUrl: 'images/tshirts.png'),
-  Category(name: 'SALE', imageUrl: 'images/sale.png'),
-  Category(name: 'Nightwear', imageUrl: 'images/nightwear.png'),
+  Category(
+      id: 'c1_sale_flash', name: 'SALE', imageUrl: 'images/flash_sale.png'),
+  Category(
+      id: 'c2_view_all', name: 'View All', imageUrl: 'images/view_all.png'),
+  Category(id: 'c3_dresses', name: 'Dresses', imageUrl: 'images/dresses.png'),
+  Category(id: 'c4_tops', name: 'Tops', imageUrl: 'images/tops.png'),
+  Category(id: 'c5_bottoms', name: 'Bottoms', imageUrl: 'images/bottoms.png'),
+  Category(id: 'c6_tshirts', name: 'T-Shirts', imageUrl: 'images/tshirts.png'),
+  Category(id: 'c7_sale', name: 'SALE', imageUrl: 'images/sale.png'),
+  Category(
+      id: 'c8_nightwear', name: 'Nightwear', imageUrl: 'images/nightwear.png'),
 ];
 
 final collectionItems = [
   CollectionItem(
-    title: 'NEW SUMMER COLLECTION 2023',
+    title: 'NEW\nSUMMER\nCOLLECTION\n2023',
     imageUrl: 'images/collection_1.png',
     buttonText: 'Shop now',
+    textColor: Colors.black,
+    buttonColor: Colors.black,
   ),
   CollectionItem(
-    title: 'NEW SUMMER COLLECTION 2023',
+    title: 'NEW\nSUMMER\nCOLLECTION\n2023',
     imageUrl: 'images/collection_2.png',
     buttonText: 'Watch now',
+    textColor: Colors.white,
+    buttonColor: Color(0xFF38BCDD),
   ),
   CollectionItem(
-    title: 'NEW SUMMER COLLECTION 2023',
+    title: 'NEW\nSUMMER\nCOLLECTION\n2023',
     imageUrl: 'images/collection_1.png',
     buttonText: 'Shop now',
+    textColor: Colors.black,
+    buttonColor: Colors.black,
+  ),
+];
+
+final List<Product> dummyProducts = [
+  Product(
+    id: 'p1_red_dress',
+    title: 'Red Dress',
+    imageUrl: 'images/red_dress.png',
+    price: 2500,
+    categories: ['c3_dresses'],
+    discount: null,
+  ),
+  Product(
+    id: 'p2_pink_dress',
+    title: 'Pink Dress',
+    imageUrl: 'images/pink_dress.png',
+    price: 2500,
+    categories: ['c1_sale_flash', 'c3_dresses'],
+    discount: 20,
+  ),
+  Product(
+    id: 'p3_gray_top',
+    title: 'Gray Top',
+    imageUrl: 'images/gray_top.png',
+    price: 2500,
+    categories: ['c1_sale_flash', 'c4_tops'],
+    discount: 20,
+  ),
+  Product(
+    id: 'p4_night_wear',
+    title: 'Night Wear',
+    imageUrl: 'images/night_wear.png',
+    price: 2500,
+    categories: ['c1_sale_flash', 'c5_bottoms'],
+    discount: 20,
+  ),
+  Product(
+    id: 'p5_yellow_dotted_dress',
+    title: 'Yellow Dotted Dress',
+    imageUrl: 'images/yellow_dress.png',
+    price: 2500,
+    categories: ['c3_dresses'],
+    discount: null,
   ),
 ];
---
+++ lib/data/top_brands.dart
@@ -0,0 +1,10 @@
+final List<String> topBrands = [
+  "images/chanel.png",
+  "images/zara.png",
+  "images/h_m.png",
+  "images/levi_s.png",
+  "images/nike.png",
+  "images/loft.png",
+  "images/h_m.png",
+  "images/h_m.png",
+];
