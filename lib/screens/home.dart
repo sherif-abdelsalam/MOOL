019a9f89967a1d08eab9136061445541950ff85f
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-23 20:43:27 +0200

    updated home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:mool/screens/best_sellers.dart';
+import 'package:mool/screens/new_arrival.dart';
 import 'package:mool/widgets/home/collections_slider.dart';
 import 'package:mool/widgets/home/delivery.dart';
-import 'package:mool/widgets/home/new_arrival.dart';
+import 'package:mool/widgets/home/discount_banner.dart';
+import 'package:mool/widgets/home/new_best_products.dart';
 import 'package:mool/widgets/home/other_collections.dart';
 import 'package:mool/widgets/home/slider_category.dart';
 import 'package:mool/widgets/home/top_brands.dart';
 import 'package:mool/widgets/reuse/custom_scaffold_header_home.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void setNewArrivalScreen() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (ctx) => NewArrivalScreen()));
+  }
+
+  void setBestSellersScreen() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (ctx) => BestSellersScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +38,17 @@ class Home extends StatelessWidget {
             SliderCategory(),
             Delivery(),
             CollectionsSlider(),
+            SizedBox(height: 32),
             OtherCollections(),
+            SizedBox(height: 32),
             TopBrands(),
-            NewArrival(),
+            NewBestProducts(
+                title: "New Arrivals", onNavigate: setNewArrivalScreen),
+            SizedBox(height: 32),
+            DiscountBanner(),
+            NewBestProducts(
+                title: "Best Sellers", onNavigate: setBestSellersScreen),
+            SizedBox(height: 64),
           ],
         ),
       ),
