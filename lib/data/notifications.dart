b0783a0b6cd66e1015d7e5ab218de2aeac859edc
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-23 20:42:05 +0200

    added dummy notifications data

---
+++ lib/data/notifications.dart
@@ -0,0 +1,22 @@
+import 'package:mool/models/notification.dart';
+
+final dummyNotifications = [
+  OneNotification(
+    id: '1',
+    message: "We thought you’d like a new winter collections",
+    timeAgo: "3 days ago",
+    imageUrl: 'images/notify.png',
+  ),
+  OneNotification(
+    id: '2',
+    message: "We thought you’d like a new winter collections",
+    timeAgo: "23 days ago",
+    imageUrl: 'images/notify.png',
+  ),
+  OneNotification(
+    id: '3',
+    message: "We thought you’d like a new winter collections",
+    timeAgo: "3 months ago",
+    imageUrl: 'images/notify.png',
+  ),
+];
