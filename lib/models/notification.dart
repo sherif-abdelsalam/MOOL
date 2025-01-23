733fe65205036d55b792b0d0ce06a6290e02c735
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-23 20:42:35 +0200

    added notification model

---
+++ lib/models/notification.dart
@@ -0,0 +1,12 @@
+class OneNotification {
+  final String id;
+  final String message;
+  final String timeAgo;
+  final String imageUrl;
+
+  OneNotification(
+      {required this.id,
+      required this.message,
+      required this.timeAgo,
+      required this.imageUrl});
+}
