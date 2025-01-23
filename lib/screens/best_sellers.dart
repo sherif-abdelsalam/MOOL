71c56333664fd41e6b3f2d8c413a478aa36428a7
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-23 20:43:01 +0200

    added best sellers screen

---
+++ lib/screens/best_sellers.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mool/data/dummy_categories.dart';
+import 'package:mool/widgets/home/product_card.dart';
+import 'package:mool/widgets/reuse/custom_scaffold_header.dart';
+
+class BestSellersScreen extends StatelessWidget {
+  const BestSellersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffoldHeader(
+      title: "Best Sellers",
+      bodyContent: Center(
+        child: _bodyContent(),
+      ),
+    );
+  }
+    Widget _bodyContent() {
+    return GridView(
+      padding: EdgeInsets.all(8),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 156 / 250,
+      ),
+      children: [for (final p in dummyProducts) ProductCard(product: p)],
+    );
+  }
+}
