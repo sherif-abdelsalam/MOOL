82ff0efcc4e908b4bbbea696a7083590b9226689
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-27 14:38:52 +0200

    added my account widgets

---
+++ lib/widgets/my_account/row_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RowContainer extends StatelessWidget {
+  const RowContainer({required this.icon, required this.text, super.key});
+
+  final Widget icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          icon,
+          SizedBox(width: 8),
+          Text(
+            text,
+            style: TextStyle(fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
