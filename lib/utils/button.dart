47b0a4fe75ec94f8cbf615569adffdff89e9629d
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-27 14:37:51 +0200

    added util button

---
+++ lib/utils/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key, required this.btnText, required this.onTapBtn});
+
+  final String btnText;
+  final void Function() onTapBtn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      width: double.infinity,
+      color: Colors.white,
+      child: ElevatedButton(
+        onPressed: onTapBtn,
+        style: ElevatedButton.styleFrom(
+          elevation: 16,
+          backgroundColor: Colors.black,
+          padding: EdgeInsets.symmetric(horizontal: 52, vertical: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Text(
+          btnText,
+          style: TextStyle(
+              color: Colors.white, fontSize: 14, fontWeight: FontWeight.w800),
+        ),
+      ),
+    );
+  }
+}
