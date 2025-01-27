1614f91ce7f372f26144c37262b373478720d1dc
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-27 21:37:48 +0200

    added payment screen

---
+++ lib/screens/Payment.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:mool/widgets/payment/payment_mathods.dart';
+
+class Payment extends StatelessWidget {
+  const Payment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "Choose Payment Method",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                PaymentMethods(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
