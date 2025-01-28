29cc1d4a7a450e5f597d7ea953e994dca51de617
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-28 14:03:41 +0200

    added checkout model

--- lib/models/cart_product.dart
+++ lib/models/cart_product.dart
@@ -3,10 +3,12 @@ import 'package:mool/models/product.dart';
 
 class CartProduct {
   CartProduct({
+    required this.id,
     required this.product,
     required this.color,
     required this.size,
   });
+  final String id;
   final Product product;
   final Color color;
   final String size;
