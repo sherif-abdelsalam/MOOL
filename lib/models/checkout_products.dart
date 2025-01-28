5bc4a83fbbe8dca509892127a88f62f7767a7879
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-01-28 14:04:17 +0200

    added checkout model

---
+++ lib/models/checkout_products.dart
@@ -0,0 +1,20 @@
+import 'package:mool/models/cart_product.dart';
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+enum Status { pending, done, none }
+
+class CheckoutProducts {
+  CheckoutProducts({
+    required this.checkoutProducts,
+    required this.subTotal,
+    required this.status,
+    this.placedDate,
+  }) : id = uuid.v4();
+  final String id;
+  final List<CartProduct> checkoutProducts;
+  final double subTotal;
+  final Status status;
+  final DateTime? placedDate;
+}
